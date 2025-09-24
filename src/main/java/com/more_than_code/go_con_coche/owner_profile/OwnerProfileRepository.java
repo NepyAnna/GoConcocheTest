36dac9515d684ab3b131619d62c245b1be1fc0f3
Nia <niaespinal@outlook.com>
2025-09-24 19:31:37 +0200

    fix: fix vehicle owner authorization checks

--- src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface OwnerProfileRepository extends JpaRepository<OwnerProfile, Long> {
     Optional<OwnerProfile> findByRegisteredUserId(RegisteredUser registeredUser);
-    boolean existsByRegisteredUser(RegisteredUser registeredUser);
+    Optional<OwnerProfile> findByRegisteredUserId(Long registeredUserId);
 }
