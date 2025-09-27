a2a1b7455d4b2c1f2f90e449b016079ef01a7253
NepyAnna <nepyann@gmail.com>
2025-09-27 15:35:06 +0200

    separated Cloudinary config and properties classesЄ

---
+++ src/main/java/com/more_than_code/go_con_coche/config/CloudinaryConfig.java
@@ -0,0 +1,27 @@
+package com.more_than_code.go_con_coche.config;
+
+import com.cloudinary.Cloudinary;
+import com.more_than_code.go_con_coche.cloudinary.DefaultImageType;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class CloudinaryConfig {
+    private final CloudinaryProperties properties;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", properties.getCloudName());
+        config.put("api_key", properties.getApiKey());
+        config.put("api_secret", properties.getApiSecret());
+        return new Cloudinary(config);
+    }
+}
